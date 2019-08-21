eec7ce6a9b5ba844a7e7838c184d72ec358f85b9
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-08-20 21:00:15 -0500

    Sanity Commit

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -3,20 +3,52 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const FriendsList = () =>{
   const [friends, setFriends] = useState(null)
+  const [credentials, setCredentials] = useState({name:'', age:'', email:''})
 
   useEffect(() => {
     axiosWithAuth()
     .get('http://localhost:5000/api/friends')
     .then(res => setFriends(res.data))
   },[friends]);
+
+  const handleChange = e => {
+    setCredentials({
+        ...credentials,
+        [e.target.name]: e.target.value
+    });
+  };
+  
+  const addFriend = e => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post('http://localhost:5000/api/friends', credentials)
+    .then(res => console.log(res))
+  
+  };
     return (
         <div>
-        {friends && friends.map(el => <h1 key={el.name}>{el.name}</h1>)}
-        <form >
+        {friends && friends.map(el => <h1 key={el.id}>{el.name}</h1>)}
+        <form onSubmit={addFriend}>
           <input
             type="text"
             name="name"
             placeholder="Name"
+            value={credentials.name}
+          onChange={handleChange}
+          />
+            <input
+            type="text"
+            name="age"
+            placeholder="Age"
+            value={credentials.age}
+          onChange={handleChange}
+          />
+            <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={credentials.email}
+          onChange={handleChange}
           />
           <button>Add Friend</button>
         </form>
@@ -24,3 +56,4 @@ const FriendsList = () =>{
         )
 }
 export default FriendsList
+
